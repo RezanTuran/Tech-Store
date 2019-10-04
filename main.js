a02cea4ee903f3deb21d43e8f76aac78ce8a55a8
Lucaspers <dunderdykaren@hotmail.com>
2019-10-04 11:41:35 +0200

    Listade produkter

--- main.js
+++ main.js
@@ -18,17 +18,42 @@ function loadProducts() {
 
 function initSite() {
     loadProducts();
-    // This would also be a good place to initialize other parts of the UI
+
 }
 
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
-    // Check your console to see that the products are stored in the listOfProducts varible.
-    console.log(listOfProducts);
 
-    // Add your code here, remember to brake your code in to smaller function blocks
-    // to reduce complexity and increase readability. Each function should have
-    // an explainetory comment like the one for this function, see row 22.
-    
-    // TODO: Remove the console.log and these comments when you've read them.
+    var body =document.getElementsByTagName("body")[0]
+    var continer = document.createElement("div");
+    continer.classList = "container";
+
+    for(var i = 0; i< listOfProducts.length; i++){
+    var selectedProduct = listOfProducts[i];
+
+    var productCard = document.createElement("div");
+    var infoList = document.createElement("ul");
+    var titlelistOfProducts = document.createElement("h4");
+    var descriptionlistOfProduct = document.createElement("p");
+    var imagelistOfProduct = document.createElement("img");
+    var pricelistOfProduct = document.createElement("li");
+
+    imagelistOfProduct.src = "./assets/" + selectedProduct.image;
+
+    titlelistOfProducts.innerText = selectedProduct.title;
+    descriptionlistOfProduct.innerText = selectedProduct.description;
+    imagelistOfProduct.innerText = selectedProduct.image;
+    pricelistOfProduct.innerText = selectedProduct.price;
+
+
+    infoList.appendChild(titlelistOfProducts);
+    infoList.appendChild(descriptionlistOfProduct);
+    infoList.appendChild(imagelistOfProduct);
+    infoList.appendChild(pricelistOfProduct);
+
+    productCard.appendChild(infoList);
+    continer.appendChild(productCard);
+}
+
+body.appendChild(continer);
 }
