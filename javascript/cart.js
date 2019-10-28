101e7ad899052942f9799468fa983097adf85d9c
Abbe91 <abdullah.abo.alhaijja@hotmail.com>
2019-10-28 10:01:39 +0100

    Totalbelopp på cart sida

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,19 +1,31 @@
 
 function getCartArray() {
-   return JSON.parse(localStorage.getItem('cart'));
+   return JSON.parse(localStorage.getItem('cart'))
 }
+
 function initSite() {
    renderProduct();
    saveProductCount();
 }
 function renderProduct(){
-   var cart = getCartArray();
-   for (var i = 0; i < cart.length; i++) {
-       var productContainer = createProductcard(cart[i]);
-       document.getElementById("mainContainer").appendChild(productContainer);
-   }
-}
-function createProductcard(product) {
+    var mainContainer = document.getElementById("mainContainer");
+    var totalPrice= document.getElementById("totalPrice");
+    mainContainer.innerHTML = "";
+    var cart = getCartArray();
+    var totalPrice = 0
+    for (var i = 0; i < cart.length; i++) {
+        var productContainer = createProductcard(cart[i], i);
+        mainContainer.appendChild(productContainer);
+        totalPrice += cart[i].price
+        document.getElementById("totalPrice").innerText = totalPrice
+      
+       
+    }
+   
+    console.log(totalPrice)
+ }
+ 
+function createProductcard(product, index) {
    var productContainer = document.createElement("div");
    productContainer.classList = "productContainer"
    var infoList= document.createElement("ul");
@@ -21,37 +33,63 @@ function createProductcard(product) {
    var descriptionlistOfProducts = document.createElement("p");
    var imagelistOfProducts = document.createElement("img");
    var pricelistOfProducts = document.createElement("p");
-   
+   var buttonlistOfProducts = document.createElement("button");
    imagelistOfProducts.classList = "imagelistOfProducts"
    titlelistOfProducts.innerText = product.title;
    descriptionlistOfProducts.innerText = product.description;
    imagelistOfProducts.innerText = product.image;
    pricelistOfProducts.innerText = product.price + " " + "KR";
-   
+   buttonlistOfProducts.innerHTML = "Delet item";
+
+   buttonlistOfProducts.onclick = function() {
+    removeProductFromCart(index)
+       
+   }
    imagelistOfProducts.src= "./assets/" + product.image;
    productContainer.appendChild(infoList);
    productContainer.appendChild(titlelistOfProducts);
    productContainer.appendChild(descriptionlistOfProducts);
    productContainer.appendChild(imagelistOfProducts);
    productContainer.appendChild(pricelistOfProducts);
-   
+   productContainer.appendChild(buttonlistOfProducts);
    return productContainer;
 }
-function addProductToCart(product) {
-   var collectedCart = JSON.parse(localStorage.getItem('cart'));
-   if(collectedCart) {
-       collectedCart.push(product);
-   } else {
-       collectedCart = [];
-       collectedCart.push(product);
+function removeProductFromCart(index) {
+    var collectedCart = getCartArray()
+    if(collectedCart) {
+        collectedCart.splice(index, 1);
+    }
+    localStorage.setItem('cart', JSON.stringify(collectedCart));
+    console.log(collectedCart)
+    renderProduct();
+ }
+ function printCartNumber() {
+    var totalPrice = document.getElementById("totalPrice");
+    var totalPrice = 0
+    for (var i = 0; i < cart.length; i++) {
+    
+        totalPrice += cart[i].price
+        }
+        console.log(totalPrice)
    }
-   document.getElementById("counter").innerHTML = collectedCart.length;
-   localStorage.setItem('cart', JSON.stringify(collectedCart));
-   
-   console.log(product)
-}
+ function ShowTotalPrice (){
+    var totalPrice = document.getElementById("totalPrice");
+    var totalPrice = 0
+    for (var i = 0; i < cart.length; i++) {
+        totalPrice.appendChild(productContainer);
+        totalPrice += cart[i].price
+        }
+        console.log(totalPrice)
 
-function saveProductCount() {
-    var collectedCart = JSON.parse(localStorage.getItem('cart'));
-    document.getElementById("counter").innerHTML = collectedCart.length;
-}
+    renderProduct();
+ }
+
+
+   function printCartNumber() {
+
+   }
+
+   function saveProductCount() {
+      var collectedCart = JSON.parse(localStorage.getItem('cart'));
+      document.getElementById("counter").innerHTML = collectedCart.length;
+  }
